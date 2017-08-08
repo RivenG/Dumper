566548f28b26943c92212063eca9ebd42906417c
Gergo Ancsicsa <gergo.ancsicsa@fotex.net>
2017-08-08 12:13:59 +0200

    Init

---
+++ src/Resources/functions/dump.php
@@ -0,0 +1,30 @@
+<?php
+
+use Riven\VarDumper\Dumper;
+
+
+if (!function_exists('d')) {
+    /**
+     * Dump the passed variables and end the script.
+     *
+     * @param  mixed
+     */
+    function d()
+    {
+        array_map(function ($x) { (new Dumper())->dump($x); }, func_get_args());
+    }
+}
+
+if (!function_exists('dd')) {
+    /**
+     * Dump the passed variables and end the script.
+     *
+     * @param  mixed
+     */
+    function dd()
+    {
+        array_map(function ($x) { (new Dumper())->dump($x); }, func_get_args());
+
+        die(1);
+    }
+}
